5a8c150cbf7cc945ba2f2e082141fdecdee4bf60
Akash <akash_shrestha01@hotmail.com>
2026-03-25 17:43:37 +0545

    feat: add ChatResponse model and update chat response to send list of ChatResponse objects

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 # import time
 from fastapi import FastAPI, Depends
 from app.ai_service import ask_ai
-from app.models import ChatRequest
+from app.models import ChatRequest, ChatResponse
 from app import database_models
 from app.database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -19,14 +19,23 @@ def get_db():
 
 @app.get("/")
 def greet():
+
     return "Welcome from fastapi"
 
-@app.get("/chat")
+@app.get("/chat", response_model=list[ChatResponse])
 def get_chat(db: Session = Depends(get_db)):
     db_chat = db.query(database_models.Chat).all()
+    response: list[ChatResponse] = []
     for message in db_chat:
-        print(message.__dict__)
-    return "db chat logged"
+        response.append(
+            {
+                "id": str(message.id),
+                "role": message.role,
+                "content": message.content
+            }
+        )
+
+    return response
 
 @app.post("/chat")
 def llm_chat(request: ChatRequest):
@@ -36,6 +45,7 @@ def llm_chat(request: ChatRequest):
     # print(ask_ai("hi"))
     ai_response  = ask_ai(request.message)
     reply = f" {ai_response}"
+
     return reply
 
 
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,10 @@
 from pydantic import BaseModel
+from uuid import UUID
 
 class ChatRequest(BaseModel):
     message: str
+
+class ChatResponse(BaseModel):
+    id: UUID
+    role: str
+    content: str
